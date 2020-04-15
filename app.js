79931f1767759d7ac01bc046760c5a6936e9db96
nikeet15 <kesharinikeet@gmail.com>
2020-04-15 16:57:59 +0530

    added new campgrounds adding page

--- app.js
+++ app.js
@@ -10,21 +10,33 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));                  //making public a static directory
 app.set("view engine", "ejs");                      //if written no need to write .ejs only write name of file
 
+var camps = [
+    { name: "Salmon Creek", image: "/image1.png" },
+    { name: "Granite Mountain", image: "/image2.png" },
+    { name: "Mountain's Rest", image: "/image3.png" },
+];
+
 //ROUTES
 app.get("/", (req, res)=>{
     res.render("landing");
 });
 
 app.get("/campgrounds", (req,res)=>{
-    var camps= [
-        { name: "Salmon Creek", image: "/image1.png" },
-        { name: "Salmon Creek", image: "/image2.png" },
-        { name: "Salmon Creek", image: "/image3.png" },
-    ]
-
     res.render("campgrounds", {camps: camps});
 });
 
+app.get("/campgrounds/new", (req, res)=>{
+    res.render("new");
+});
+
+app.post("/campgrounds", (req,res)=>{
+    var name= req.body.name;
+    var image= req.body.image;
+    var newcamp= {name: name, image: image};
+    camps.push(newcamp);
+    
+    res.redirect("/campgrounds");
+})
 
 //starting server code..............................
 app.listen(3000, function () {
