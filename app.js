45987769fb0614e220d74638976dd79d1968768d
nikeet15 <kesharinikeet@gmail.com>
2020-04-17 02:39:28 +0530

    completed post route for adding new comments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require("express");                                // express() re
 var mongoose = require("mongoose");
 var bodyparser = require("body-parser");
 var Campground = require("./models/campground")
+var Comment = require("./models/comment");
 var seedDB= require("./seeds");
 
 var app = express();
@@ -77,7 +78,7 @@ app.get("/campgrounds/:id", (req,res)=>{                               // note c
 
 });
 
-//Coments routes
+// Coments routes
 app.get("/campgrounds/:id/comments/new", (req,res)=>{
     Campground.findById(req.params.id, (err, foundCampground)=>{
         if(err)
@@ -85,11 +86,35 @@ app.get("/campgrounds/:id/comments/new", (req,res)=>{
         
         else{
             console.log("found camp- "+ foundCampground);
+            console.log("rendering to new comments adding page");
             res.render("./comments/new", {campground: foundCampground});
         }
     })
-    
-    
+});
+
+app.post("/campgrounds/:id/comments", (req,res)=>{
+    // create a new comment
+    // find campground and push comment to it
+    // redirect to campground show page
+
+    Campground.findById(req.params.id, (err, foundCampground)=>{
+        if(err)
+            console.log("error in finding a camp");
+
+        else{
+            console.log("found camp- ", foundCampground);
+            Comment.create(req.body.comment, function(err, newComment){
+                if(err)
+                    console.log("error in creating comment");
+
+                else{
+                    foundCampground.comments.push(newComment);
+                    foundCampground.save();
+                    res.redirect("/campgrounds/" +foundCampground._id);
+                }
+            });
+        }
+    });
 });
 
 
